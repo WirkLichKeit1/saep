ea85f461e12f53fe002ddd37eff6807f6978c2ac
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-11-13 19:34:34 -0300

    a

--- saep/controllers/dbController.py
+++
@@ -1,8 +0,0 @@
-from repositories.BaseRepository import get_db
-from flask import Blueprint
-
-db_bp = Blueprint("db", __name__)
-
-@db_bp.route("/db", methods=["GET"])
-def get_conn():
-    return get_db()
